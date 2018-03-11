dc49360a85ad1f499e8af8836e6b73740cab1a26
TELHOOSIN-LAB-D\taher <taher.elhossin@symbyo.com>
2018-03-11 23:10:22 +0200

    add commints

--- contracts/MyTestContract.sol
+++ contracts/MyTestContract.sol
@@ -9,19 +9,30 @@ contract MyTestContract {
     uint256 public total;
     address owner;
 
+    // Construct to just track my contruct owner
     function MyTestContract() {
         owner =  msg.sender;
     }
 
+    /**
+    send function take what ever value in the message and track it in total variable 
+    Note: value of the message automatically added to contract public address global balance
+     */
     function takeMony() public payable {       
         total += msg.value; 
     }
 
+    /**
+    Traverse balance of contract from glabal state not UTXO (thank you doctor Sameh :D )
+     */
     function getBalance() public constant returns(uint256) {        
         return this.balance;
     }
 
-    function payMonyFor(address add1) public payable {
+    /**
+    Transfer money to sent address
+     */
+    function payMoneyFor(address add1) public payable {
         require(msg.sender == owner);       
         add1.transfer(total); 
                    
